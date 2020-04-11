4b00f4668df534b865b80efab51e5b78b6d5043a
fieldchester <37942267+fieldchester@users.noreply.github.com>
2020-04-11 06:08:02 +0200

    Update 00listMonad.js

--- 00listMonad.js
+++ 00listMonad.js
@@ -1,28 +1,55 @@
-// Monad für ein Array
-// -------------------
+// Array Monad
+// -----------
 
+// map
+// (a -> b) -> ma -> mb
 
-// fmap mit vertauschten Argumenten
-// []a -> (a -> b) -> []b
-// [].map(f)
+// Kleisli,  monadisches compose (.)
+// (a->[]b) -> (b->[]c) -> a -> []c
+const fish = (f, g) => x => f(x).bind(g)
 
+// bind
+// []a -> (a -> []b) -> []b
+Array.prototype.bind = function(f) {
+    return this.reduce(
+      (acc, x) => acc.concat(f(x)), [] )}
 
 // of, return
 // a -> []a
 const of = a => Array.of(a)
 
 
-// bind
-// []a -> (a -> []b) -> []b
-Array.prototype.bind = function(f) {
-  return this.reduce(
-    (acc, x) => acc.concat(f(x)), [] )}
 
 
-// >>
-// []a -> []b -> []b
-Array.prototype.skip = function(b) {
-  return this.length === 0
-    ? []
-    : this.reduce(
-        (acc) => acc.concat(b), [] )}
+
+// Examples
+// I use VSCode und Quokka (shows console.log's dirctly in Editor
+
+// Character to list of Characters
+// 'b' -> ['a', 'b']
+const c2cs = c => Array.from({length: s2n(c)}, (v,i) => n2s(i))
+
+// Number to list of Numbers
+// 2 -> [1, 2]
+const n2ns = n => Array.from({length: n}, (v,i) => i+1 )
+
+
+
+// with fish
+const c2ns = fish(x => c2cs(x),
+             fish(y => n2ns(s2n(y)),
+             of))
+console.log(c2ns("c"))
+
+// with bind
+const ns = c2cs("c").bind( y =>
+           n2ns(s2n(y)).bind(
+           of))
+console.log(ns)
+
+
+
+// "a" -> 1, "b" -> 2...
+function s2n(s) { return parseInt(s, 36) - 9 }
+// 1 -> "a", 2 -> "b", ...
+function n2s(n) { return String.fromCharCode(97+n) }
