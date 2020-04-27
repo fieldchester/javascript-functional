3d523cd0f907211a05de2ccbb3a96cd1772cfa8e
fieldchester <37942267+fieldchester@users.noreply.github.com>
2020-04-27 22:38:32 +0200

    Update 02listMonadDiffType.js

--- 02listMonadDiffType.js
+++ 02listMonadDiffType.js
@@ -6,19 +6,15 @@ function composeK (f, g) { return function (x) { return f(x).then(g)  }  }
 // Array Monad
 // -----------
 
-// Misstook types:
-// (a->[]b) -> (c->[]d) -> a -> []d
+// Thought, I misstook types:
+// (a->[b]) -> (c->[d]) -> a -> [d]
 // instead of
 // (a->[b]) -> (b->[c]) -> a -> [c]
-// but it works
 
-
-
-// I use VSCode und Quokka (shows console.logs directly in Editor)
-
-const composed = composeK(a =>
-    c2cs(a), composeK(b =>
-    n2ns(   s2n(b)  ), x => of(x) ))    // s2n the type conversion
+// Then was surprised to have to do type conversion
+const composed = composeK(a => c2cs(a)
+                , composeK(b => n2ns(   s2n(b)  )   // s2n the type conversion
+                , x => of(x) ))   
 
 console.log(composed("c"))
 
@@ -29,20 +25,28 @@ const ns = c2cs("c").then( a =>
             of(y)))
 console.log(ns)
 
+// And finally realised, that (_->[b]) -> (c->_) is not possible with
+// polymrophic composeK / then. It would only be possible with e.g.:
+// comoposeKChar2Int.
+// The "typ conversion" is even reqired in the x->[y] Kleisli arrows.
 
 
 // Helper
 
 // Character to list of Characters
 // 'b' -> ['a', 'b']
+// String -> [String]
 function c2cs(c) { return Array.from({length: s2n(c)}, (v,i) => n2s(i)) }
 
 // Number to list of Numbers
 // 2 -> [1, 2]
+// Int -> [Int]
 function n2ns(n) { return Array.from({length: n}, (v,i) => i+1 ) }
 
 // "a" -> 1, "b" -> 2...
+// String -> Int
 function s2n(s) { return parseInt(s, 36) - 9 }
 
 // 1 -> "a", 2 -> "b", ...
+// Int -> String
 function n2s(n) { return String.fromCharCode(97+n) }
