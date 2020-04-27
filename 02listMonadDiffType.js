ccfdbb7327a1b16c71ebc8992a4d2a30c33160c0
fieldchester <37942267+fieldchester@users.noreply.github.com>
2020-04-27 21:32:48 +0200

    Update and rename 00listMonadDiffType.js to 02listMonadDiffType.js

---
+++ 02listMonadDiffType.js
@@ -0,0 +1,55 @@
+Array.prototype.flatten = function() { return this.reduce((acc, x) => acc.concat(x), [] )}
+function of(a) { return Array.of(a) }
+Array.prototype.then = function(f) { return this.map(f).flatten() }
+function composeK (f, g) { return function (x) { return f(x).then(g)  }  }
+
+// Array Monad
+// -----------
+
+// Misstook types:
+// (a->[]b) -> (c->[]d) -> a -> []d
+// instead of
+// (a->[b]) -> (b->[c]) -> a -> [c]
+// but it works
+
+
+
+// I use VSCode und Quokka (shows console.log's directly in Editor)
+
+
+// with composeK
+// (a->[]b) -> (c->[]d) -> a -> []d
+// const c2ns = composeK(x =>
+//     c2cs(x), composeK(y =>
+//     n2ns(s2n(y)) , z => of(z))))
+
+const c2ns = composeK(a =>
+    c2cs(a), composeK(b =>
+    n2ns(   s2n(b)  ), x => of(x) ))    // s2n the type conversion
+
+console.log(c2ns("c"))
+
+// with bind
+// []a -> (b -> []c) -> []c
+const ns = c2cs("c").then( x =>
+            n2ns(   s2n(x)  ).then(y =>   // s2n the type conversion
+            of(y)))
+console.log(ns)
+
+
+
+// Helper
+
+// Character to list of Characters
+// 'b' -> ['a', 'b']
+function c2cs(c) { return Array.from({length: s2n(c)}, (v,i) => n2s(i)) }
+
+// Number to list of Numbers
+// 2 -> [1, 2]
+function n2ns(n) { return Array.from({length: n}, (v,i) => i+1 ) }
+
+// "a" -> 1, "b" -> 2...
+function s2n(s) { return parseInt(s, 36) - 9 }
+
+// 1 -> "a", 2 -> "b", ...
+function n2s(n) { return String.fromCharCode(97+n) }
